b9baf991109db3ff98d62d3fc4bb141b89a5812f
Caickk <145877432+Caickk@users.noreply.github.com>
2025-05-12 15:49:36 -0300

    partially done gui

--- newGui.py
+++ newGui.py
@@ -36,9 +36,11 @@ frame._set_appearance_mode('dark')
 frame.title('Cliente')
 frame.geometry('600x800')
 
-userID = ctk.CTkLabel(frame,text=("ID do veiculo " + vehicle.ID + " "))
+#id do veiculo
+userID = ctk.CTkLabel(frame,text=("" + vehicle.ID + " "))
 userID.pack(pady=20)
 
+#info
 battery_info_text = ctk.StringVar()
 battery_info = ctk.CTkLabel(frame,textvariable=battery_info_text)
 battery_info.pack(pady=10)
@@ -47,48 +49,77 @@ critical_battery_text = ctk.StringVar()
 critical_battery = ctk.CTkLabel(frame,textvariable=critical_battery_text)
 critical_battery.pack(pady=20)
 
-spotRequestButton = ctk.CTkButton(frame,text=' Obter a distância até a estação de recarga mais próxima e o preço do KWh ',command=lambda:vehicle.nearestSpotRequest())
-spotRequestButton.pack(pady=10)
 
-distance_info_text = ctk.StringVar()
-distance_info = ctk.CTkLabel(frame,textvariable=distance_info_text)
-distance_info.pack(pady=20)
-
-simulatePayButton = ctk.CTkButton(frame,text=' Gerar guia de pagamento ',command=lambda:vehicle.simulateForNearestSpot())
-simulatePayButton.pack(pady=10)
-
-next_purchase_info_text = ctk.StringVar()
-next_purchase_info = ctk.CTkLabel(frame,textvariable=next_purchase_info_text)
-next_purchase_info.pack(pady=20)
-
-bookButton = ctk.CTkButton(frame,text=' Recarregar totalmente na estação mais próxima ',command=lambda:vehicle.payForNearestSpot())
-bookButton.pack(pady=10)
-
-next_purchase_result_text = ctk.StringVar()
-next_purchase_result = ctk.CTkLabel(frame,textvariable=next_purchase_result_text)
-next_purchase_result.pack(pady=30)
-
-originPlaceholder = ctk.CTkEntry(frame,placeholder_text='Digite o Servidor de origem')
+originPlaceholder = ctk.CTkEntry(frame,placeholder_text='Digite o Servidor de origem',width=180)
 originPlaceholder.pack(pady=10)
-
-destinationPlaceholder = ctk.CTkEntry(frame,placeholder_text='Digite o Servidor de destino')
-destinationPlaceholder.pack(pady=10)
 
 validateServersButton = ctk.CTkButton(frame,text=' Selecionar servidores ',command=getServerPlaceholders) #precisa da referência da função correta
 validateServersButton.pack(pady=20)
+#TopLevelStatus
 
+#Recharge StringVar
+distance_info_text = ctk.StringVar()
+next_purchase_info_text = ctk.StringVar()
+next_purchase_result_text = ctk.StringVar()
+#History StringVar
 purchaseHistoryID = ctk.StringVar()
 purchaseHistoryTotal = ctk.StringVar()
 purchaseHistoryPrice = ctk.StringVar()
 purchaseHistoryCharge = ctk.StringVar()
-#Frame2 = Gerenciador de Rotas
+#Route StringVar
+routeSelectionResult = ctk.StringVar()
+#frame4 = Gerenciador de Recarga
 
+def closeAny(frame):
+    frame.destroy()
+    frame.update()
+
+def openRechargeManager():
+
+
+    frame4 = ctk.CTkToplevel(frame) 
+    frame4.title('Gerenciar Recarga')
+    frame4.geometry('600x800')
+    frame4.attributes('-topmost',True)
+
+    spotRequestButton = ctk.CTkButton(frame4,text=' Obter a distância até a estação de recarga mais próxima e o preço do KWh ',command=lambda:vehicle.nearestSpotRequest())
+    spotRequestButton.pack(pady=10)
+
+    distance_info = ctk.CTkLabel(frame4,textvariable=distance_info_text)
+    distance_info.pack(pady=20)
+
+    simulatePayButton = ctk.CTkButton(frame4,text=' Gerar guia de pagamento ',command=lambda:vehicle.simulateForNearestSpot())
+    simulatePayButton.pack(pady=10)
+    
+    next_purchase_info = ctk.CTkLabel(frame4,textvariable=next_purchase_info_text)
+    next_purchase_info.pack(pady=20)
+
+    bookButton = ctk.CTkButton(frame4,text=' Recarregar totalmente na estação mais próxima ',command=lambda:vehicle.payForNearestSpot())
+    bookButton.pack(pady=10)
+
+    next_purchase_result = ctk.CTkLabel(frame4,textvariable=next_purchase_result_text)
+    next_purchase_result.pack(pady=30)
+
+    def closeRRMWindow():
+        frame4.destroy()
+        frame4.update()
+    closeRRButton = ctk.CTkButton(frame4,text=' Fechar Gerenciador de Recargas ',command=closeRRMWindow)
+    closeRRButton.pack(pady=20)
+
+openRM = ctk.CTkButton(frame,text=' Recarregar ',command=openRechargeManager)
+openRM.pack(pady=20)
+
+#Frame2 = Gerenciador de Rotas
 def openRechargeRouteManager():
+
     frame2 = ctk.CTkToplevel(frame)
     frame2.title('Gerenciar Recargas na Rotas')
     frame2.geometry('600x800')
     frame2.attributes('-topmost',True)
     
+    destinationPlaceholder = ctk.CTkEntry(frame2,placeholder_text='Digite o Servidor de destino',width=180)
+    destinationPlaceholder.pack(pady=10)
+
     #comandos a serem definidos
     backButton = ctk.CTkButton(frame2,text=' < ')
     backButton.pack(pady=5)
@@ -96,15 +127,23 @@ def openRechargeRouteManager():
     forwardButton = ctk.CTkButton(frame2,text=' > ')
     forwardButton.pack(pady=20)
     
+    selectRouteButton = ctk.CTkButton(frame2,text=' Selecionar Rota ')
+    selectRouteButton.pack(pady=5)
+
+    route_selection_Result = ctk.CTkLabel(frame2,textvariable=routeSelectionResult)
+    route_selection_Result.pack(pady=30)
     def closeRRMWindow():
         frame2.destroy()
         frame2.update()
     closeRRMButton = ctk.CTkButton(frame2,text=' Fechar Gerenciador de Rotas ',command=closeRRMWindow)
     closeRRMButton.pack(pady=20)
+
 openRRMButton = ctk.CTkButton(frame,text=' Gerenciar Recarga na Rota ',command=openRechargeRouteManager)
 openRRMButton.pack(pady=10)
+
 #frame3 = histórico
 def openHistoryWindow():
+
     frame3 = ctk.CTkToplevel(frame)
     frame3.title('Histórico')
     frame3.geometry('400x600')
@@ -140,6 +179,7 @@ def openHistoryWindow():
     
 openHistoryButton = ctk.CTkButton(frame,text=' Abrir Histórico ',command=openHistoryWindow)
 openHistoryButton.pack(pady=20)
+
 
     
 frame.mainloop()
